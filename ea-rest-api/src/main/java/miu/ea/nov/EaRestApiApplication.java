abdb0f86f8211ce84ea4a7230b3c41fefd4ac7cd
dunky-star <dunkygeoffrey39@gmail.com>
2023-10-31 23:41:24 -0500

    Add n-tier architecture - Spring Boot starter jar files

---
+++ ea-rest-api/src/main/java/miu/ea/nov/EaRestApiApplication.java
@@ -0,0 +1,13 @@
+package miu.ea.nov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaRestApiApplication.class, args);
+	}
+
+}
