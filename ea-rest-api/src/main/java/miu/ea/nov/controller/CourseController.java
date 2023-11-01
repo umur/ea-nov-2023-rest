93cacb04c9ed13d2c4c1f539f09ce59bceafb240
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-01 15:38:38 -0500

    Completed course logic

--- ea-rest-api/src/main/java/miu/ea/nov/controller/CourseController.java
+++ ea-rest-api/src/main/java/miu/ea/nov/controller/CourseController.java
@@ -1,8 +1,13 @@
 package miu.ea.nov.controller;
 
+import miu.ea.nov.payload.CourseDto;
 import miu.ea.nov.service.impl.CourseServiceImpl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController // Combination of @Controller and @ResponseBody annotations
 @RequestMapping("/api/courses") // Base URL for REST API
@@ -18,5 +23,15 @@ public class CourseController {
     Implement CRUD operations for the domains.
     Implement getStudentsByMajor(String major)
     Returns all courses of the student.*/
+
+    @GetMapping("/find")
+    public List<CourseDto> findAllCourses() {
+        return courseService.findAll();
+    }
+
+    @PostMapping("/create")
+    public void addCourse(CourseDto courseDto) {
+
+    }
 
 } // End of course controller
