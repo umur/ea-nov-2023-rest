a5369ded8b3c96a4ba60b3a6c5f6ee197b4ebcbf
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-02 17:38:39 -0500

    Added ModeMapper dependency and defined it as Spring Bean

--- ea-rest-api/src/main/java/miu/ea/nov/EaRestApiApplication.java
+++ ea-rest-api/src/main/java/miu/ea/nov/EaRestApiApplication.java
@@ -1,10 +1,17 @@
 package miu.ea.nov;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EaRestApiApplication {
+
+	@Bean // To make ModelMapper a Spring Bean.
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EaRestApiApplication.class, args);
